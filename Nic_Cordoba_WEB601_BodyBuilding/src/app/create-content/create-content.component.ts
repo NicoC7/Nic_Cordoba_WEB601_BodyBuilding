409e83251744735f30bd39ad0a1292a4bdc52e58
NicoC7 <W0823791@myscc.ca>
2024-03-01 13:09:20 -0500

    Adding the form and the function to add items(not working)

--- Nic_Cordoba_WEB601_BodyBuilding/src/app/create-content/create-content.component.ts
+++ Nic_Cordoba_WEB601_BodyBuilding/src/app/create-content/create-content.component.ts
@@ -1,14 +1,25 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-content',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-content.component.html',
   styleUrl: './create-content.component.scss'
 })
 export class CreateContentComponent {
 
-  
+  @Output() contentAdded = new EventEmitter<Content>();
+  newContent: Content = { id: 0, title: '', description: '', creator: '', imageURL: '', tags: [], type: '' };
+  tagInput: string = '';
 
+  addContent(): void {
+    const tags = this.tagInput.split(',').map(tag => tag.trim());
+    this.newContent.tags = tags;
+    this.contentAdded.emit(this.newContent);
+    this.newContent = { id: 0, title: '', description: '', creator: '', imageURL: '', tags: [], type: '' };
+    this.tagInput = '';
+  }
 }
